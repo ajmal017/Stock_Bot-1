1e9eedae8a408d5a2ce6b6c4b25f23048bf3c5c4
Aksu1 <50671877+Aksu1@users.noreply.github.com>
2020-07-29 22:21:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ from dhooks import Webhook
 from datetime import datetime
 
 # Configs
-hook = Webhook("https://discordapp.com/api/webhooks/734768699973828618/aaWXsr1s2AMuWNxTxM_eruEc5363IasVGpWLlnuMxUasjIJ3QW0gjJpbs4JZ_6muccY8")  # webhook that we are using to post live trades
+hook = Webhook("https://discordapp.com/api/webhooks/729645128591081513/3sSDtFLES3R-yV2YBtbqLJC_98OsIROfiAuvNbloX9LiQd81LeKFwxjhFgxfBajQ8dr-")  # webhook that we are using to post live trades
 app = Chalice(app_name='api')  # define app
-API_KEY = 'PKYZ7D8EVVMATC0A50Y6'  # alpaca api key used for authentication
-SECRET_KEY = '1MMJ7RVvYkz7pHrD62k0f9iEdK0gveSHwu0e4hWk'  # alpaca secret key used for authentication
+API_KEY = 'PKORFSQ5J7FQXN28S20F'  # alpaca api key used for authentication
+SECRET_KEY = 'TKjVdKzNLgxDoKYk8qr6vuAXYwBUjshmiOaxw6LL'  # alpaca secret key used for authentication
 BASE_URL = "https://paper-api.alpaca.markets"  # alpaca url that we are using to post data to
 ORDERS_URL = "https://paper-api.alpaca.markets/v2/orders"  # alpaca url that we are using to post data to
 POSITIONS_URL = "https://paper-api.alpaca.markets/v2/positions"  # alpaca url that we are using to post data to
@@ -18,7 +18,7 @@ ASSETS_URL = "https://paper-api.alpaca.markets/v2/assets"  # alpaca url that we 
 ACCOUNT_URL = "https://paper-api.alpaca.markets/v2/account"  # alpaca url that we are using to post data to
 HISTORY_URL = "https://paper-api.alpaca.markets/v2/account/portfolio/history"  # alpaca url that we are using to post data to
 HEADERS = {'APCA-API-KEY-ID': API_KEY, 'APCA-API-SECRET-KEY': SECRET_KEY}  # headers that contains our keys
-STARTING_BALANCE = 12500  # starting equity
+STARTING_BALANCE = 35000  # starting equity
 STOCKS = 1  # enter the amount of stocks we are currently trading
 
 # Api verification
@@ -50,7 +50,7 @@ def buy_stock():
         side = wh_message['side']  # get side from webhook message (either long entry / long stop loss / long take profit | short entry / short stop loss / short take profit)
         symbol_bars = api.get_barset(symbol, 'minute', 1).df.iloc[0]  # get live price from alpaca
         symbol_price = symbol_bars[symbol]['close']  # get live price from alpaca
-        quantity = round(((float(account.buying_power) / float(wh_message['price'])) / STOCKS) * 0.97)  # divide current available buying power with price of the stock divide that by the amount of possible stocks we are currently buying and finally multiply it by 0.97 this calculation will calculate how many stocks we can buy with 97% of our total equity
+        quantity = round(((float(account.buying_power) / float(wh_message['price'])) / STOCKS) * 0.95)  # divide current available buying power with price of the stock divide that by the amount of possible stocks we are currently buying and finally multiply it by 0.97 this calculation will calculate how many stocks we can buy with 97% of our total equity
         balance_change = float(account.equity) - float(account.last_equity)  # compares current equity to yesterdays equity to get the daily profit margin
         total_profit = float(account.equity) - STARTING_BALANCE  # compares current equity to starting balance to get the total profit margin
         total_percentage = 100 * ((float(account.equity) - float(STARTING_BALANCE)) / float(STARTING_BALANCE))   # calculates the total percentage gain
@@ -61,7 +61,9 @@ def buy_stock():
                   "error occurred" + "\n" +
                   "```")
         return
-
+    print(quantity)
+    print(symbol_price)
+    print(total_price)
     # LONG ENTRY
     # LONG ENTRY
     # LONG ENTRY
@@ -88,7 +90,7 @@ def buy_stock():
                       "+ " + "Quantity: " + response['qty'] + "\n" +
                       "+ " + str('Total Price: ' + '$-{:,.2f}'.format(float(total_price))) + "\n" +
                       "+ " + str('Current Available Funds: ' + '${:,.2f}'.format(float(account.equity))) + "\n" +
-                      "+ " + str('Current Available Buying Power: ' + '${:,.2f}'.format(float(account.buying_power))) + "\n" +
+                      "+ " + str('Current Available Buying Power: ' + '${:,.2f}'.format(float(account.buying_power) - float(total_price))) + "\n" +
                       "+ " + str('Today\'s Profit: ' + '${:,.2f}'.format(float(balance_change)) + " (" + '{:.2f}%'.format(float(today_percentage)) + ")") + "\n" +
                       "+ " + str('Total Profit: ' + '${:,.2f}'.format(float(total_profit)) + " (" + '{:.2f}%'.format(float(total_percentage)) + ")") + "\n" +
                       "+ " + "ID: " + response['id'] + "\n" +
@@ -129,7 +131,7 @@ def buy_stock():
                       "+ " + "Quantity: " + response['qty'] + "\n" +
                       "+ " + str('Total Price: ' + '$-{:,.2f}'.format(float(total_price))) + "\n" +
                       "+ " + str('Current Available Funds: ' + '${:,.2f}'.format(float(account.equity))) + "\n" +
-                      "+ " + str('Current Available Buying Power: ' + '${:,.2f}'.format(float(account.buying_power))) + "\n" +
+                      "+ " + str('Current Available Buying Power: ' + '${:,.2f}'.format(float(account.buying_power) - float(total_price))) + "\n" +
                       "+ " + str('Today\'s Profit: ' + '${:,.2f}'.format(float(balance_change)) + " (" + '{:.2f}%'.format(float(today_percentage)) + ")") + "\n" +
                       "+ " + str('Total Profit: ' + '${:,.2f}'.format(float(total_profit)) + " (" + '{:.2f}%'.format(float(total_percentage)) + ")") + "\n" +
                       "+ " + "ID: " + response['id'] + "\n" +
@@ -147,7 +149,7 @@ def buy_stock():
     # EXIT
     # EXIT
     # EXIT
-    if side == 'short take profit' or side == 'short stop loss' or side == 'long take profit' or side == 'long stop loss':
+    if side == 'short take profit' or side == 'short stop loss' or side == 'long take profit' or side == 'long stop loss' or side == 'exit timer':
         r = requests.delete(POSITIONS_URL + "/" + wh_message['symbol'], headers=HEADERS)  # deletes the order (aka liquidate/sell)
         response = json.loads(r.content)  # get the response
         print(response)  # print the response
@@ -161,9 +163,9 @@ def buy_stock():
                       "- " + "Type: " + wh_message['side'] + "\n" +
                       "- " + "Price: " + '$' + str(symbol_price) + "\n" +
                       "- " + "Quantity: " + response['qty'] + "\n" +
-                      "- " + str('Total Price: ' + '$+{:,.2f}'.format(float(total_price))) + "\n" +
+                      "- " + str('Total Price: ' + '$+{:,.2f}'.format(float(symbol_price) * float(response['qty']))) + "\n" +
                       "- " + str('Current Available Funds: ' + '${:,.2f}'.format(float(account.equity))) + "\n" +
-                      "- " + str('Current Available Buying Power: ' + '${:,.2f}'.format(float(account.buying_power))) + "\n" +
+                      "- " + str('Current Available Buying Power: ' + '${:,.2f}'.format(float(account.buying_power) + float(total_price))) + "\n" +
                       "- " + str('Today\'s Profit: ' + '${:,.2f}'.format(float(balance_change)) + " (" + '{:.2f}%'.format(float(today_percentage)) + ")") + "\n" +
                       "- " + str('Total Profit: ' + '${:,.2f}'.format(float(total_profit)) + " (" + '{:.2f}%'.format(float(total_percentage)) + ")") + "\n" +
                       "- " + "ID: " + response['id'] + "\n" +
@@ -178,6 +180,6 @@ def buy_stock():
                       "X " + "Error: " + response['message'] + "\n" +
                       "```")
     return {
-        'message': 'Success!',
+        'message': 'I bought the stock!',
         'wh_message': wh_message
     }
